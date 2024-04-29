bca4fffac9051030dee39c7afc5e22bbdf1c68a4
McNeil McCarley <mcneilmccarley@yahoo.com>
2024-04-29 17:11:54 -0500

    Fixed urls.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
-# Create your views here.
+def product_index(request):
+    products = Product.objects.all()
+    return render(request, 'myapp/product_index.html', {'products': products})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'myapp/product_detail.html', {'product': product})
